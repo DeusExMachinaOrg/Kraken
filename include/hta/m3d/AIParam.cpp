0a51364f2ca2c5f53b533db9ee8b4d376fd1d410
Alexander <foggy1989@gmail.com>
2025-11-14 01:22:18 +0400

    Remove any hta related headers

--- include/hta/m3d/AIParam.cpp
+++
@@ -1,8 +0,0 @@
-#include "hta/m3d/AIParam.hpp"
-
-namespace m3d {
-    void AIParam::Detach() {
-        FUNC(0x00404FF0, void, __thiscall, _Detach, const AIParam*);
-        _Detach(this);
-    };
-}
