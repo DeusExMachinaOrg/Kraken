a9bdb53c6fa4decc198d248597ffa50071e15c6b
Alexander <foggy1989@gmail.com>
2025-12-19 12:18:14 +0400

    Explicit stdlib in a `stdafx.h`

--- source/stdafx.hpp
+++ source/stdafx.hpp
@@ -1,6 +1,17 @@
 #ifndef __KRAKEN_STDAFX_HPP__
 #define __KRAKEN_STDAFX_HPP__
 
+#define NOMINMAX
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+
 #include <memory>
 #include <stdexcept>
 
